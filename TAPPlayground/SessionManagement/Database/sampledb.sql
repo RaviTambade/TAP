29788911cea3100102998cd5c07cae8f8579bab8
unknown <rushikeshchikane101@gmail.com>
2023-03-02 23:16:28 +0530

    DB Update

--- TAPPlayground/SessionManagement/Database/sampledb.sql
+++ TAPPlayground/SessionManagement/Database/sampledb.sql
@@ -22,3 +22,40 @@ VALUES ('Maggi', 'Ready To Cook', 12,12000,12000,0),
 
 use simpledb;
 SELECT * FROM products;
+
+create table customers(custId int not null auto_increment primary key,
+                      firstName varchar(255),
+                      lastName varchar(255),
+                      email varchar (255) unique,
+                      contact varchar(20) unique,
+                      permanentAdd varchar(255));
+                      
+ insert into customers(firstName,lastName,email,contact,permanentAdd)values('Rohit', 'Gore','rohitG123@gmail.com','759862356','Secrete HartTown,Wanavdi,Hadapsar,Pune');
+ 
+ select * from customers;
+ 
+ create table orders(orderId int not null auto_increment primary key,
+				     orderDate datetime,
+                     custId int  not null,
+					 constraint fk_customerId foreign key (custId) references customers(custId) on update CASCADE,
+                     status varchar(200)
+                     );
+ 
+insert into orders(orderDate,custId,status)values(now(),1,'Approved');    
+
+select * from orders;
+
+create table orderDetails( orderDetailsId int not null auto_increment primary key,
+						   orderId int  not null,
+					       constraint fk_orderId foreign key (orderId) references orders(orderId) on update CASCADE,
+                           productId int  not null,
+					       constraint fk_productId foreign key (productId) references products(productId) on update CASCADE,
+                           quantity int  not null
+                           );
+                           
+insert into orderDetails(orderId,productId,quantity) values(1,2,50);    
+
+select * from orders
+where custId in(select custId from customers where custId=1);
+
+     
