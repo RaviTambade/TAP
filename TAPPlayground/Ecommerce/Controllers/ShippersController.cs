da10dbdeb9621217500fc254d5421865423f3308
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-03-15 08:45:31 +0530

    CRUD Opration of Shipper

---
+++ TAPPlayground/Ecommerce/Controllers/ShippersController.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceApp.Models;
+using ECommerceApp.Services.Interfaces;
+
+namespace ECommerceApp.Controllers;
+
+public class ShipperController : Controller
+{
+    private readonly IShipperService _shippersrv;
+
+    public ShipperController(IShipperService shippersrv)
+    {
+        _shippersrv = shippersrv;
+    }
+
+    [HttpGet]
+    public JsonResult GetAllShippers()
+    {
+        var shippers=_shippersrv.GetAllShippers();
+        return Json(shippers);
+    }
+
+    [HttpGet]
+    public JsonResult GetShipperById(int id)
+    {
+        var shipper=_shippersrv.GetShipperById(id);
+        return Json(shipper);
+    }
+
+
+   [HttpPost]
+    public JsonResult InsertShipper([FromBody]Shipper shipper)
+    {
+        bool status=_shippersrv.InsertShipper(shipper);
+        return Json(status);
+    }
+
+
+
+
+
+    [HttpPut]
+    public JsonResult UpdateShipper([FromBody]Shipper shipper)
+    {
+        System.Console.WriteLine(shipper);
+        bool status=_shippersrv.UpdateShipper(shipper);
+        return Json(status);
+
+    }
+
+
+
+
+
+   [HttpDelete]
+    public JsonResult DeleteShipper(int id)
+    {
+        bool status=_shippersrv.DeleteShipper(id);
+        return Json(status);
+}
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
