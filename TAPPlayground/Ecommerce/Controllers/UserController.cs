1e1eeadedbc560001b845d0877f285c00f408766
unknown <gore.rohit1902@gmail.com>
2023-03-18 01:54:27 +0530

    User Home Page View

---
+++ TAPPlayground/Ecommerce/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceApp.Models;
+using ECommerceApp.Services.Interfaces;
+
+namespace ECommerceApp.Controllers;
+
+public class UserController : Controller
+{
+    private readonly IUserService _usersrv;
+
+    public UserController(IUserService usersrv)
+    {
+        _usersrv = usersrv;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public JsonResult ShowAllUser()
+    {
+        var details = _usersrv.GetAllUser();
+        return Json(details);
+    }
+}
