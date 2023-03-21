7de4a56b287e632a01c05006c5a5c15bcb451ba4
unknown <ravi.tambade@transflower.in>
2023-03-21 15:41:30 +0530

    Angular app

---
+++ TAPPlayground/EComUIApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+//metadata
+//decorator
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent] //Root Component
+})
+export class AppModule { } //Root Module
+
+//What do you mean by Angular Module ?
+//it is like namespace  or package
+//it consist of
+//Basic building blocks:
+// components, services, directives, pipes, decorators
+// interface,classes
