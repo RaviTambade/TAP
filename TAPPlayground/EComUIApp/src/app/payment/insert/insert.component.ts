8b9664d20ad8ffcd6a9d3aaa994654477556d1b0
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-03-28 10:30:29 +0530

    paymentComponent

---
+++ TAPPlayground/EComUIApp/src/app/payment/insert/insert.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Payment } from '../payment';
+import { PaymenthubService } from '../paymenthub.service';
+
+@Component({
+  selector: 'app-insert',
+  templateUrl: './insert.component.html',
+  styleUrls: ['./insert.component.css']
+})
+export class InsertComponent implements OnInit {
+
+  payment: Payment ={
+    paymentId:0,
+    paymentDate:'',
+    paymentMode:'',
+    transactionId:0,
+    orderId:0
+
+  }
+status :boolean |undefined
+// model={
+
+//  " paymentId":0,
+//     "paymentDate":'',
+//     "paymentMode":"",
+//     "transactionId":'',
+//     "orderId":''
+// };
+constructor(private svc:PaymenthubService){}
+
+
+ngOnInit(): void {
+
+}
+
+insert(form:any){
+
+  this.svc.insert(form).subscribe(
+    (response)=>{
+      this.status=response;
+      console.log(response)
+    }
+  )
+
+}
+
+
+}
