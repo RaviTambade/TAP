fca85422def9e57b886919f82d8eabbc8088b701
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-04-05 21:16:21 +0530

    Shipper

--- TAPPlayground/JwtTokan/JwtTokan/Models/Shipper.cs
+++ TAPPlayground/JwtTokan/JwtTokan/Models/Shipper.cs
@@ -9,6 +9,7 @@ public class Shipper{
       public string? ContactNumber{get;set;}
 
       public string? Email{get;set;}
+      public string? Password{get;set;}
 
       public long AccountNumber{get;set;}
 
