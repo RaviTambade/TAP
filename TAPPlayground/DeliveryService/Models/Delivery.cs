073f581e8d3eaeef5091abe057f2f4952bed4d16
unknown <ravi.tambade@transflower.in>
2023-04-11 15:30:15 +0530

    Delivery tracking

--- TAPPlayground/DeliveryService/Models/Delivery.cs
+++ TAPPlayground/DeliveryService/Models/Delivery.cs
@@ -6,5 +6,4 @@ public class Delivery{
       public string? Email{get;set;}
       public string? Password{get;set;}
       public long AccountNumber{get;set;}
-
 }
--- TAPPlayground/GateWay/Program.cs
+++ TAPPlayground/GateWay/Program.cs
@@ -3,15 +3,12 @@ using Ocelot.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Configuration.AddJsonFile("configuration.json", optional:false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -20,10 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 await app.UseOcelot();
-
 app.Run();
