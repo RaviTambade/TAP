93e8f21884ec4d664aaf2062149ec5321a4c91dc
PragatiBangar <bangarpragati11@gmail.com>
2023-04-13 18:19:28 +0530

    Roles based authentication added

--- TAPPlayground/OrderProcessingTestApp/Program.cs
+++ TAPPlayground/OrderProcessingTestApp/Program.cs
@@ -1,12 +1,16 @@
 ﻿using OrderProcessingService.Models;
 using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
 class Program
 {
     private static readonly HttpClient client = new HttpClient();
     static async Task Main(string[] args)
     {
-        await FetchOrdersFromRESTAPI();
-        await FetchOrderDetails();
+        //await FetchOrdersFromRESTAPI();
+        //await FetchOrderDetails();
+        await InsertOrder();
+        await UpdateOrder();
        
     }
      public static async Task FetchOrdersFromRESTAPI()
@@ -48,37 +52,34 @@ class Program
       private static async Task InsertOrder()
     {
         Console.WriteLine("Enter OrderDate:");
-        string orderDate = Console.ReadLine();
+        DateTime orderDate = Convert.ToDateTime(Console.ReadLine());
 
-        Console.WriteLine("Enter ShiipedDate:");
-        string shippedDate = Console.ReadLine();
+        Console.WriteLine("Enter ShippedDate:");
+        DateTime shippedDate = Convert.ToDateTime(Console.ReadLine());
 
-        Console.WriteLine("Enter Customer Email");
-        string email = Console.ReadLine();
+        Console.WriteLine("Enter Customer Id:");
+        int customerId = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter Customer Contact Number");
-        string contactNumber = Console.ReadLine();
+        Console.WriteLine("Enter Total:");
+        double total = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter Customer Password");
-        string password = Console.ReadLine();
+        Console.WriteLine("Enter order Status:");
+        string status = Console.ReadLine();
 
-        Console.WriteLine("Enter Customer Account Number");
-        long accountNumber = long.Parse(Console.ReadLine());
-
-        Customer customer = new Customer()
+        Order order = new Order()
         {
-            FirstName = firstName,
-            LastName = lastName,
-            Email = email,
-            ContactNumber = contactNumber,
-            Password = password,
-            AccountNumber = accountNumber
+            OrderDate = orderDate,
+            ShippedDate = shippedDate,
+            CustomerId= customerId,
+            Total = total,
+            Status= status
+            
         };
-        string jsonCustomer = System.Text.Json.JsonSerializer.Serialize(customer);
+        string jsonCustomer = System.Text.Json.JsonSerializer.Serialize(order);
         var requestContent = new StringContent(jsonCustomer, Encoding.UTF8,"application/json");
         using (var httpClient = new HttpClient())
         {
-            string apiUrl = "http://localhost:4002/api/customers/addcustomer";
+            string apiUrl = "http://localhost:5158/addorder";
             using (var response = await httpClient.PostAsync(apiUrl, requestContent))
             {
                response.EnsureSuccessStatusCode();
@@ -86,5 +87,29 @@ class Program
             }
         }
     }
+    // private static async Task UpdateOrder()
+    // {
+    //     Console.WriteLine("Enter your OrderId for updating");
+    //     int OrderId = int.Parse(Console.ReadLine());
+    //     Order order = new Order();
+
+    //     Console.WriteLine("enter new orderdate:");
+    //     DateTime orderDate = Convert.ToDateTime(Console.ReadLine());
+
+    //     Console.WriteLine("enter new shippeddate:");
+    //     DateTime shippedDate = Convert.ToDateTime(Console.ReadLine());
+
+    //     Console.WriteLine("enter new customerId:");
+    //     int customerId = Convert.ToInt32(Console.ReadLine());
+
+    //     Console.WriteLine("enter new customerId:");
+    //     double total = Convert.ToInt32(Console.ReadLine());
+
+    //     Console.WriteLine("enter new order status:");
+    //     string status = Console.ReadLine();
+        
+
+
+    // }
 }
 
